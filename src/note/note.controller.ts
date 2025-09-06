8069bd20a71424e4f54caa75b15b99edc00c7406
Gabriel Guerra Kageyama <gabe.kage1@gmail.com>
2025-09-06 19:14:32 -0300

    fixing mongoose related bugs

--- src/note/note.controller.ts
+++ src/note/note.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, UseGuards } from "@nestjs/common";
 import { NoteService } from "./note.service";
 import { JwtGuard } from "src/auth/guards";
 import { GetUser } from "src/auth/decorators";
@@ -12,26 +12,32 @@ import { ParseObjectIdPipe } from "@nestjs/mongoose";
 export class NoteController {
     constructor(private noteService: NoteService) { }
     
+    @HttpCode(HttpStatus.OK)
     @Get(':id')
     getNote( @GetUser('') user: User, @Param('id', ParseObjectIdPipe) noteId: Object ) {
         return this.noteService.getNote(user, noteId);
     }
 
+    @HttpCode(HttpStatus.OK)
     @Get('user-notes')
     getUserNotes( @GetUser('') user: User){
         return this.noteService.getUserNotes(user);
     }
-    
+
+    @HttpCode(HttpStatus.CREATED)
     @Post('create-note')
     createNote( @GetUser('') user: User, @Body() dto: NoteDto ) {
         return this.noteService.createNote(user, dto);
     }
 
+    @HttpCode(HttpStatus.OK)
     @Patch(':id')
     updateNote( @GetUser('') user: User, @Body() dto: UpdateNoteDto, @Param('id', ParseObjectIdPipe) noteId: Object ) {
         return this.noteService.updateNote(user, dto, noteId);
     }
 
+
+    @HttpCode(HttpStatus.NO_CONTENT)
     @Delete(':id')
     deleteNote( @GetUser('') user: User, @Param('id', ParseObjectIdPipe) noteId: Object) {
         return this.noteService.deleteNote(user, noteId);
--- src/note/note.service.ts
+++ src/note/note.service.ts
@@ -1,4 +1,4 @@
-import { ForbiddenException, Injectable } from "@nestjs/common";
+import { ForbiddenException, HttpCode, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Note } from "Schemas/note.schema";
@@ -7,7 +7,8 @@ import { NoteDto, UpdateNoteDto } from "./dto";
 
 @Injectable()
 export class NoteService {
-    constructor(@InjectModel(Note.name) private noteModel: Model<Note>) { }
+    constructor(@InjectModel(Note.name) private noteModel: Model<Note>,
+                @InjectModel(User.name) private userModel: Model<User>) { }
 
     async getNote(user: User, noteId: Object) {
         return this.noteModel.findById(noteId);
@@ -18,9 +19,11 @@ export class NoteService {
     }
 
     async createNote(user: User, dto: NoteDto) {
+        const noteCreator = await this.userModel.findOne({ username: user.username }).select('+hashPw');
+        
         const note = new this.noteModel ({
             ...dto,
-            noteCreator: user
+            noteCreator
         });
         await note.save();
         return note;
@@ -35,15 +38,14 @@ export class NoteService {
 
     async deleteNote(user: User, noteId: Object) {
         const note = await this.noteModel.findById(noteId);
+        
 
-        if(!note || note.noteCreator != user ){
+        if(!note || note.noteCreator.username != user.username ){ //should be better work with the user id, but because username is also unique i will mantain like this for now
             throw new ForbiddenException (
                 'Unable to delete desired information'
             );
         }
 
-        note.deleteOne(noteId);
-
-        return 'Deletion completed';
+        await note.deleteOne(noteId);
     }
 }
